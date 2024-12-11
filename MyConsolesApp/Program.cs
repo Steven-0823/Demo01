1a13e1b360261def6e5e6dbd01f655a04962213a
Joan Henao <henaobermeosteven@gmail.com>
2024-12-11 16:38:23 -0500

    Inicio Proyecto

---
+++ MyConsolesApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyConsolesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
